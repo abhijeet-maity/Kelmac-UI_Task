6388bf285ba21948e7b01187b04684b062919ac7
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-03-05 17:24:40 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from "../../images/kelmac_logo.png";
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo} alt="" className='logo'/>
+        <div className='right-section'>
+            <h3>+91 98920 98920</h3>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/">About us</Link></li>
+                <li><Link to="/">Courses</Link></li>
+                <li><Link to="/">Training</Link></li>
+            </ul>
+            <button className='btn'>Contact us</button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
