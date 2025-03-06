7f06bdfd60c2835bd6689468b0ab53e9057f286e
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-03-06 17:46:08 +0530

    changes done and file created

---
+++ src/components/Page2/Page2.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Page2.css";
+
+
+const Page2 = () => {
+  return (
+    
+      <section className="page2">
+        <section className="text-container">
+          <div>
+            <h2>Customized programs tailored to client requirements!</h2>
+            <p>Fully Tailored to Your Business Needs</p>
+          </div>
+          <div>
+            <h3>No two businesses are the same</h3>
+            <p>
+              so why settle for a one-size-fits-all training program? Kelmac
+              Group’s private training is designed specifically for you.
+            </p>
+          </div>
+          <div>
+            <h3>Customized to Your Industry & Company Processe</h3>
+            <p>
+              We take the time to understand your organization, your industry,
+              and your unique challenges. Our courses are then tailored to focus
+              on the specific skills and knowledge your team needs.
+            </p>
+          </div>
+          <div>
+            <h3>Relevant, Real-World Training</h3>
+            <p>
+              Instead of covering theoretical concepts that may not apply to
+              your team, our training integrates real-world scenarios, case
+              studies, and practical exercises related to your company’s
+              operations.
+            </p>
+          </div>
+          <div>
+            <h3>Targeted Learning for Different Teams</h3>
+            <p>
+              Whether you need to train senior management, internal auditors, or
+              frontline employees, we adjust the content to ensure each group
+              gets the most value from the session.
+            </p>
+          </div>
+        </section>
+      </section>
+ 
+  );
+};
+
+export default Page2;
---
+++ src/components/Page3/Page3.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Page3.css";
+import TrainingCardContainer from "../TrainingCards/TrainingCardContainer/TrainingCardContainer.jsx";
+
+const Page3 = () => {
+  return (
+    <main >
+        <TrainingCardContainer/>
+    </main>
+  )
+}
+
+export default Page3
