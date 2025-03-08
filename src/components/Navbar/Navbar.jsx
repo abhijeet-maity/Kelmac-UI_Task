b8f0d47f823ae791da66d82d145d386d9d963265
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-03-08 10:21:32 +0530

    added phone icon, added hover property to links and buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React from "react";
 import logo from "../../images/Group 1.png";
-import { Link } from 'react-router-dom';
+import phoneIcon from "../../images/phoneIcon.png";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-        <img src={logo} alt="" className='logo'/>
-        <div className='right-section'>
-            <h3>+91 98920 98920</h3>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/">About us</Link></li>
-                <li><Link to="/">Courses</Link></li>
-                <li><Link to="/">Training</Link></li>
-            </ul>
-            <button className='btn'>Contact us</button>
+      <img src={logo} alt="" className="logo" />
+      <div className="right-section">
+        <div className="phone-number">
+          <img src={phoneIcon} alt="" />
+          <h3>+91 98920 98920</h3>
         </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">About us</Link>
+          </li>
+          <li>
+            <Link to="/">Courses</Link>
+          </li>
+          <li>
+            <Link to="/">Training</Link>
+          </li>
+        </ul>
+        <button className="btn">Contact us</button>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
